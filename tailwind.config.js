ea46d5a36b8a9d7af40b042b61a5712b04526694
vehktaur <victorakhihiero@gmail.com>
2024-06-19 10:33:17 +0100

    omo... plenty changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,9 @@ export default {
     extend: {
       backgroundImage: {
         'm-footer':
-          "url('./src/assets/footer_bg_tl.png'),url('./src/assets/footer_bg_br.png'),url('./src/assets/footer_bg_r.png')",
+          "url('/src/assets/footer_bg_tl.png'),url('/src/assets/footer_bg_br.png'),url('/src/assets/footer_bg_r.png')",
         'md-footer':
-          "url('./src/assets/md_footer_bg_bl.png'),url('./src/assets/md_footer_bg_tr.png'),url('./src/assets/md_footer_bg_r.png')"
+          "url('/src/assets/md_footer_bg_bl.png'),url('/src/assets/md_footer_bg_tr.png'),url('/src/assets/md_footer_bg_r.png')"
       },
       backgroundPosition: {
         'footer-positions': 'top left, bottom right, bottom right',
@@ -29,8 +29,8 @@ export default {
         'orenda-green': '#127801'
       },
       content: {
-        email: 'url("./src/assets/dark_email.svg")',
-        phone: 'url("./src/assets/dark_call.svg")',
+        email: 'url("/src/assets/dark_email.svg")',
+        phone: 'url("/src/assets/dark_call.svg")',
       },
       fontFamily: {
         'open-sans': ['"Open Sans"', 'sans-serif'],
@@ -38,7 +38,7 @@ export default {
         'dm-sans': ['"DM Sans"', 'sans-serif']
       },
       listStyleImage: {
-        dot: 'url("./src/assets/disc.svg")'
+        dot: 'url("/src/assets/disc.svg")'
       },
       screens: {
         forLabel: '23.75rem',
