d3b5e4de5c04d93ac7b9e5c143577b990668c721
vehktaur <victorakhihiero@gmail.com>
2024-07-17 22:14:37 +0100

    changes

--- src/components/Our Team/OurTeamIntro.jsx
+++ src/components/Our Team/OurTeamIntro.jsx
@@ -1,7 +1,7 @@
 const OurTeamIntro = () => {
   return (
     <div className="~mt-4/[5.5rem] px-5">
-      <h1 className="heading mb-4">Meet Our Providers</h1>
+      <h1 className="heading ~mb-2/4">Meet Our Providers</h1>
       <p className="text-justify ~w-[15rem]/full mx-auto text-black ~text-sm/xl max-w-7xl">
       We have assembled a team of over 70+ experienced, board-certified providers across various specialties to ensure you receive the best possible care. Our diverse team is dedicated to supporting your growth and well-being. If at any point you feel another provider may better meet your needs, rest assured that you have the flexibility to choose the care that's right for you.
       </p>
