798e35a484feb044ad01499f2e98b553b4df5e43
vehktaur <victorakhihiero@gmail.com>
2024-07-25 13:18:15 +0100

    changes to positions in team data

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -27,177 +27,175 @@ const teamData = [
     position: 'Billing Manager'
   },
   {
-    image: teamImages.darianny,
-    name: 'Darianny',
+    image: teamImages.righteous,
+    name: 'Righteous',
     position: 'Credentialing Manager'
   },
   {
-    image: teamImages.righteous,
-    name: 'Righteous',
-    position: 'Comms Coordinator'
+    image: teamImages.progress,
+    name: 'Progress',
+    position: 'Tech Solutions Manager'
+  },
+  {
+    image: teamImages.darianny,
+    name: 'Darianny',
+    position: 'Client Support Specialist'
   },
   {
     image: teamImages.sylvia,
     name: 'Sylvia',
-    position: 'Intake Coordinator'
+    position: 'Intake Specialist'
   },
   {
-    image: teamImages.rhei,
-    name: 'Rhei',
-    position: 'Admin Member'
-  },
-  {
-    image: teamImages.valerie,
-    name: 'Valerie',
-    position: 'Admin Member'
-  },
-
-  {
-    image: teamImages.progress,
-    name: 'Progress',
-    position: 'Admin Member'
-  },
-  {
-    image: teamImages.precious,
-    name: 'Precious',
-    position: 'Admin Member'
-  },
-  {
-    image: teamImages.patty,
-    name: 'Patty',
-    position: 'Admin Member'
+    image: teamImages.shaina,
+    name: 'Shaina',
+    position: 'Intake Specialist'
   },
   {
     image: teamImages.nora,
     name: 'Nora',
-    position: 'Admin Member'
+    position: 'Intake Specialist'
   },
   {
-    image: teamImages.nikko,
-    name: 'Nikko',
-    position: 'Admin Member'
+    image: teamImages.precious,
+    name: 'Precious',
+    position: 'Intake Specialist'
   },
   {
-    image: teamImages.neil,
-    name: 'Neil',
-    position: 'Admin Member'
+    image: teamImages.cj,
+    name: 'CJ',
+    position: 'Client Support Specialist'
   },
-
+  {
+    image: teamImages.valerie,
+    name: 'Valerie',
+    position: 'Client Support Specialist'
+  },
+  {
+    image: teamImages.rhei,
+    name: 'Rhei',
+    position: 'Client Support Specialist'
+  },
+  {
+    image: teamImages.ime,
+    name: 'Ime',
+    position: 'Intake Specialist'
+  },
+  {
+    image: teamImages.oly,
+    name: 'Oly',
+    position: 'Intake Specialist'
+  },
+  {
+    image: teamImages.liza,
+    name: 'Liza',
+    position: 'Clinical Support Specialist'
+  },
   {
     image: teamImages.kevin,
     name: 'Kevin',
-    position: 'Admin Member'
+    position: 'Intake Specialist'
   },
 
   {
     image: teamImages.justine,
     name: 'Justine',
-    position: 'Admin Member'
-  },
-  {
-    image: teamImages.john_patrick,
-    name: 'John Patrick',
-    position: 'Admin Member'
-  },
-  {
-    image: teamImages.jo,
-    name: 'Jo',
-    position: 'Admin Member'
-  },
-  {
-    image: teamImages.jerricoh,
-    name: 'Jerricoh',
-    position: 'Admin Member'
-  },
-  {
-    image: teamImages.jerimine,
-    name: 'Jerimine',
-    position: 'Admin Member'
-  },
-  {
-    image: teamImages.jeremie,
-    name: 'Jeremie',
-    position: 'Admin Member'
-  },
-  {
-    image: teamImages.jaylo,
-    name: 'Jaylo',
-    position: 'Admin Member'
-  },
-  {
-    image: teamImages.itoro,
-    name: 'Itoro',
-    position: 'Admin Member'
-  },
-  {
-    image: teamImages.gene,
-    name: 'Gene',
-    position: 'Admin Member'
-  },
-  {
-    image: teamImages.emmeline,
-    name: 'Emmeline',
-    position: 'Admin Member'
+    position: 'Intake Specialist'
   },
   {
     image: teamImages.ella,
     name: 'Ella',
-    position: 'Admin Member'
+    position: 'Biller'
   },
   {
     image: teamImages.carlos,
     name: 'Carlos',
-    position: 'Admin Member'
+    position: 'Clinical Support Specialist'
   },
   {
     image: teamImages.angel,
     name: 'Angel',
-    position: 'Admin Member'
+    position: 'Client Support Specialist'
   },
   {
-    image: teamImages.ime,
-    name: 'Ime',
-    position: 'Admin Member'
+    image: teamImages.patty,
+    name: 'Patty',
+    position: 'Client Support Specialist'
   },
   {
-    image: teamImages.oly,
-    name: 'Oly',
-    position: 'Admin Member'
-  },
-  {
-    image: teamImages.shaina,
-    name: 'Shaina',
-    position: 'Admin Member'
-  },
-  {
-    image: teamImages.liza,
-    name: 'Liza',
-    position: 'Admin Member'
-  },
-  {
-    image: teamImages.phoebe,
-    name: 'Phoebe',
-    position: 'Admin Member'
-  },
-  {
-    image: teamImages.jirah,
-    name: 'Jirah',
-    position: 'Admin Member'
-  },
-  {
-    image: teamImages.cj,
-    name: 'CJ',
-    position: 'Admin Member'
+    image: teamImages.nikko,
+    name: 'Nikko',
+    position: 'Credentialing Specialist'
   },
   {
     image: teamImages.fretzie,
     name: 'Fretzie',
-    position: 'Admin Member'
+    position: 'Credentialing Specialist'
+  },
+  {
+    image: teamImages.jirah,
+    name: 'Jirah',
+    position: 'Credentialing Specialist'
+  },
+  {
+    image: teamImages.neil,
+    name: 'Neil',
+    position: 'Client Specialist'
+  },
+  {
+    image: teamImages.john_patrick,
+    name: 'John Patrick',
+    position: 'Clinical Support Specialist'
+  },
+  {
+    image: teamImages.jo,
+    name: 'Jo',
+    position: 'Biller'
+  },
+  {
+    image: teamImages.jerricoh,
+    name: 'Jerricoh',
+    position: 'Client Support Specialist'
+  },
+  {
+    image: teamImages.jerimine,
+    name: 'Jerimine',
+    position: 'Client Support Specialist'
+  },
+  {
+    image: teamImages.jeremie,
+    name: 'Jeremie',
+    position: 'Credentialing Specialist'
+  },
+  {
+    image: teamImages.jaylo,
+    name: 'Jaylo',
+    position: 'Intake Specialist'
+  },
+  {
+    image: teamImages.itoro,
+    name: 'Itoro',
+    position: 'Intake Specialist'
+  },
+  {
+    image: teamImages.phoebe,
+    name: 'Phoebe',
+    position: 'Intake Specialist'
+  },
+  {
+    image: teamImages.gene,
+    name: 'Gene',
+    position: 'Intake Specialist'
+  },
+  {
+    image: teamImages.emmeline,
+    name: 'Emmeline',
+    position: 'Intake Specialist'
   },
   {
     image: teamImages.jessabeth,
     name: 'Jessabeth',
-    position: 'Admin Member'
+    position: 'Client Support Specialist'
   }
 ];
 
