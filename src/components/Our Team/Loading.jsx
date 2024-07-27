6fb6d2487245e934f3f97928e0ada7bbf10d64c7
vehktaur <victorakhihiero@gmail.com>
2024-07-27 20:39:14 +0100

    style Loading

--- src/components/Our Team/Loading.jsx
+++ src/components/Our Team/Loading.jsx
@@ -1,9 +1,9 @@
 const Loading = ({ data }) => {
   return (
-    <div className="min-h-[35rem] grid place-items-center ~text-lg/xl">
+    <div className="min-h-[35rem] grid place-items-center ~text-lg/xl font-medium font-dm-sans italic">
       <p className="flex flex-col items-center gap-5">
         Fetching {data}...{' '}
-        <span className="block bg-transparent border-l-[0.1rem] border-black size-12 animate-spin rounded-full"></span>{' '}
+        <span className="block bg-transparent border-l-[0.1rem] border-orenda-purple size-12 animate-spin rounded-full"></span>{' '}
       </p>
     </div>
   );
